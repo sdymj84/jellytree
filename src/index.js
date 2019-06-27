a4e274081e3a99472b543b76733786700a4089e3
Minjun Youn <minjun1.y@partner.samsung.com>
2019-06-27 13:58:41 -0500

    Update HashRouter basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <HashRouter>
+  <HashRouter basename="/">
     <App />
   </HashRouter>
   , document.getElementById('root'));
