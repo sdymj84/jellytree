10c19b6c27c9f027752d60792f147464c16db038
Minjun Youn <minjun1.y@partner.samsung.com>
2019-06-27 12:00:56 -0500

    Updates

---
+++ precache-manifest.33f1dd7e4164bf6bd5c7f629f7245922.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8c29f8bddc89ec47bb7b0e9e6b9feeb",
+    "url": "/jellytree/index.html"
+  },
+  {
+    "revision": "4ddd061d62704d805a16",
+    "url": "/jellytree/static/css/main.4c9976cd.chunk.css"
+  },
+  {
+    "revision": "1e94514e6eb6c71d49a1",
+    "url": "/jellytree/static/js/2.6a9a34c4.chunk.js"
+  },
+  {
+    "revision": "4ddd061d62704d805a16",
+    "url": "/jellytree/static/js/main.2565fc43.chunk.js"
+  },
+  {
+    "revision": "4459760670b669a56a23",
+    "url": "/jellytree/static/js/runtime~main.191db9d7.js"
+  },
+  {
+    "revision": "793f3699e234f5687b58d02520f73589",
+    "url": "/jellytree/static/media/baby_main.793f3699.jpg"
+  },
+  {
+    "revision": "ce0c68b64ec3a89fd3e7e2624aa53994",
+    "url": "/jellytree/static/media/collections_main.ce0c68b6.jpg"
+  },
+  {
+    "revision": "6eff882c11e372a7dadb3fa8c541e279",
+    "url": "/jellytree/static/media/girls_main.6eff882c.jpg"
+  },
+  {
+    "revision": "7ef56667835b6d52c73a989ab206e31c",
+    "url": "/jellytree/static/media/home_main.7ef56667.jpg"
+  },
+  {
+    "revision": "4412f4c8d0f5a8861b9dbe8f314c5268",
+    "url": "/jellytree/static/media/jellytree_logo.4412f4c8.jpg"
+  },
+  {
+    "revision": "ce91d7513ada16ac33719376ffc1b769",
+    "url": "/jellytree/static/media/protection.ce91d751.jpg"
+  },
+  {
+    "revision": "01d7b482e30846c84e2fabc3838b64da",
+    "url": "/jellytree/static/media/softness.01d7b482.jpg"
+  },
+  {
+    "revision": "c594ae9e4593725f38979c4eedba1a7a",
+    "url": "/jellytree/static/media/style.c594ae9e.jpg"
+  },
+  {
+    "revision": "aed24cf1045d20d6ecddb9973cece2ca",
+    "url": "/jellytree/static/media/toddler_main.aed24cf1.jpg"
+  }
+]);
