979217f529ddd2ebf0aa1b1a06fe2b0bbc5af2c0
Minjun Youn <minjun1.y@partner.samsung.com>
2019-07-03 10:59:41 -0500

    Updates

---
+++ precache-manifest.29b83dfe48624f33c7444ac0fe200937.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6228ec36d1deb8160f485857b0600923",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bdcacf3acbc0c1599af",
+    "url": "/static/css/main.4c9976cd.chunk.css"
+  },
+  {
+    "revision": "c34ec48e57a0bda04888",
+    "url": "/static/js/2.9626b4d2.chunk.js"
+  },
+  {
+    "revision": "3bdcacf3acbc0c1599af",
+    "url": "/static/js/main.9e932027.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "793f3699e234f5687b58d02520f73589",
+    "url": "/static/media/baby_main.793f3699.jpg"
+  },
+  {
+    "revision": "ce0c68b64ec3a89fd3e7e2624aa53994",
+    "url": "/static/media/collections_main.ce0c68b6.jpg"
+  },
+  {
+    "revision": "449b18053ea06792c1f91224476e0f3e",
+    "url": "/static/media/dot-bonnet.449b1805.jpg"
+  },
+  {
+    "revision": "6eff882c11e372a7dadb3fa8c541e279",
+    "url": "/static/media/girls_main.6eff882c.jpg"
+  },
+  {
+    "revision": "7ef56667835b6d52c73a989ab206e31c",
+    "url": "/static/media/home_main.7ef56667.jpg"
+  },
+  {
+    "revision": "4412f4c8d0f5a8861b9dbe8f314c5268",
+    "url": "/static/media/jellytree_logo.4412f4c8.jpg"
+  },
+  {
+    "revision": "ce91d7513ada16ac33719376ffc1b769",
+    "url": "/static/media/protection.ce91d751.jpg"
+  },
+  {
+    "revision": "01d7b482e30846c84e2fabc3838b64da",
+    "url": "/static/media/softness.01d7b482.jpg"
+  },
+  {
+    "revision": "c594ae9e4593725f38979c4eedba1a7a",
+    "url": "/static/media/style.c594ae9e.jpg"
+  },
+  {
+    "revision": "aed24cf1045d20d6ecddb9973cece2ca",
+    "url": "/static/media/toddler_main.aed24cf1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jellytree/precache-manifest.9b13455f0247cbda40ab9b47c7337c83.js"
+  "/precache-manifest.29b83dfe48624f33c7444ac0fe200937.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jellytree/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
