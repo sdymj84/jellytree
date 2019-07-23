d7e86d73f187238780610756a77cee910e4c9505
Minjun Youn <minjun1.y@partner.samsung.com>
2019-07-23 17:47:30 -0500

    Update setProducts for batch update

--- functions/products/setProducts.js
+++ functions/products/setProducts.js
@@ -1,13 +1,26 @@
 const cors = require('cors')({ origin: true })
 const db = require('../database')
+const uuidv1 = require('uuid/v1')
 
 const setProducts = async (req, res) => {
   cors(req, res, async () => {
     try {
       const data = req.body
       console.log(data)
-      const result = await db.collection('products').doc(data.id).set(data)
+
+      // Get a new write batch
+      const batch = db.batch();
+
+      data.map(product => {
+        // Set the value of each product
+        let productRef = db.collection('products').doc(product.id || uuidv1());
+        batch.set(productRef, product);
+      })
+
+      // Commit the batch
+      const result = await batch.commit()
       res.status(200).json(result)
+
     } catch (e) {
       const msg = "Error setting documents"
       console.log(msg, e)
