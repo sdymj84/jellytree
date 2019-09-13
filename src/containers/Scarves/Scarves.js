6802827ea60672f3dd3866a239570004ed50ad3b
Minjun Youn <minjun1.y@partner.samsung.com>
2019-09-13 14:11:55 -0500

    Update Jumbo image slide

--- src/containers/Scarves/Scarves.js
+++ src/containers/Scarves/Scarves.js
@@ -5,6 +5,10 @@ import DownArrowIcon from '../../components/DownArrowIcon'
 import Products from '../Products/Products'
 
 const Container = styled.div`
+  .vanish {
+    animation: vanish-animation 0.2s ease;
+  }
+
 `
 const Jumbotron = styled.div`
   display: flex;
@@ -18,6 +22,7 @@ const Jumbotron = styled.div`
   background-position: center;
   background-size: cover;
   background-attachment: fixed;
+
 `
 const Title = styled.div`
   font-size: 5em;
@@ -30,15 +35,21 @@ const Scarves = () => {
   const [isScrollDownClicked, setIsScrollDownClicked] = useState(() =>
     sessionStorage.getItem('isScarvesRemoved')
   )
+
+  const [isVanishing, setIsVanishing] = useState(false)
   const handleScrollDown = () => {
-    setIsScrollDownClicked(true)
-    sessionStorage.setItem('isScarvesRemoved', true)
+    setIsVanishing(true)
+    setTimeout(() => {
+      setIsScrollDownClicked(true)
+      sessionStorage.setItem('isScarvesRemoved', true)
+    }, 200);
+
   }
 
   return (
     <Container>
       {!isScrollDownClicked &&
-        <Jumbotron>
+        <Jumbotron className={isVanishing ? "vanish" : ""}>
           <Title>Wear the Jelly</Title>
           <DownArrowIcon handleScrollDown={handleScrollDown} />
         </Jumbotron>}
